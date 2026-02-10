b92c00da5fe0eb7b7d604cfe3c05c968d7608061
Edouard CHIN <chin.edouard@gmail.com>
2026-02-11 00:40:29 +0100

    Gem bootstra

---
+++ lib/precompiled_gems.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "precompiled_gems/version"
+
+module PrecompiledGems
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "precompiled_gems"
+
+require "minitest/autorun"
