8f6ae2a1bcafed17ca51c68d53e3449a57486f8d
Edouard CHIN <chin.edouard@gmail.com>
2026-02-11 15:57:43 +0100

    Direct dependency hijack

--- lib/precompiled_gems.rb
+++ lib/precompiled_gems.rb
@@ -1,8 +1,14 @@
 # frozen_string_literal: true
 
 require_relative "precompiled_gems/version"
+require_relative "precompiled_gems/dsl_patch"
 
 module PrecompiledGems
-  class Error < StandardError; end
-  # Your code goes here...
+  extend self
+
+  def list
+    {
+      "bigdecimal" => "ed-precompiled_bigdecimal",
+    }
+  end
 end
